42f623e546d0966e44debc859ca8c65d93bf454e
calvin1564 <cyoung02@uoguelph.ca>
2019-10-04 15:36:54 -0400

    added sensor test sketch

---
+++ Sensor_Test/Sensor_Test.ino
@@ -0,0 +1,31 @@
+#include <SparkFunBME280.h>
+#include <SparkFunLSM6DS3.h>
+#include <SPI.h>
+#include <Wire.h>
+
+LSM6DS3 IMU ( SPI_MODE, 10);
+BME280 ATMOSPHERIC_SENSOR;
+
+void setup(){
+  Serial.begin(115200);
+  delay(500);
+  Wire.begin();
+  Serial.println("Testing Sensor Connections");
+
+  if(ATMOSPHERIC_SENSOR.beginI2C() != 0){
+    Serial.println("Error connecting to Atmospheric Sensor via i2c, check connections");
+  }
+  else{
+    Serial.println("Atmospheric sensor available!");
+  }
+
+  if(IMU.begin() != 0){
+    Serial.println("Error connecting to the IMU via SPI at CS=10, check connections");
+  }
+  else{
+    Serial.println("IMU Available!");
+  }
+  
+}
+
+void loop(){}
